3bdeeed7e9b7b74c088d405f99b4b96268874310
Simon Rojas <srojas@mahisoft.com>
2018-01-27 10:52:40 -0500

    Add exception handling and lemmatization

--- a1_preproc.py
+++ a1_preproc.py
@@ -4,7 +4,9 @@ import json
 import os
 import re
 import sys
+
 import spacy
+import spacy.tokens
 
 # TODO Restore this!
 # TODO Restore this!
@@ -27,16 +29,36 @@ wordlists_dir = prefix_wordlist + 'Wordlists/'
 nlp = spacy.load('en', disable=['parser', 'ner'])
 
 
+def validate_input(text):
+    if not text:
+        raise RuntimeWarning("Not processing an empty string")
+
+
 def remove_newlines(text):
-    return text.replace("\n", " ").replace("\r", " ")
+    try:
+        validate_input(text)
+        return text.replace("\n", " ").replace("\r", " ")
+    except RuntimeWarning as e:
+        # print("Warning: {}".format(e))
+        return text
 
 
 def remove_urls(text):
-    return re.sub(r'(\bhttps?:?//[\w./\-:]+)|(\bwww\.[\w./\-:]+)', repl='', string=text)
+    try:
+        validate_input(text)
+        return re.sub(r'(https?:?//[\w./\-:]+)|(www\.[\w./\-:]+)', repl='', string=text)
+    except RuntimeWarning as e:
+        # print("Warning: {}".format(e))
+        return text
 
 
 def remove_html_char_codes(modComm):
-    return html.unescape(modComm)
+    try:
+        validate_input(modComm)
+        return html.unescape(modComm)
+    except RuntimeWarning as e:
+        # print("Warning: {}".format(e))
+        return modComm
 
 
 def read_all_abbreviations():
@@ -74,31 +96,40 @@ def read_stopwords():
 
 
 def split_punctuation(modComm):
-    abbreviations = read_all_abbreviations()
+    try:
+        validate_input(modComm)
+        abbreviations = read_all_abbreviations()
 
-    abbreviations_regex = "(" + "|".join(abbreviations).replace(".", "\.") + ")"
+        abbreviations_regex = "(" + "|".join(abbreviations).replace(".", "\.") + ")"
 
-    number_with_separator_regex = r"\d{1,3}(,\d{3})+(\.\d+)?"
-    number_without_separator_regex = r"\b\d+\b"
+        number_with_separator_regex = r"\d{1,3}(,\d{3})+(\.\d+)?"
+        number_without_separator_regex = r"\b\d+\b"
 
-    # TODO Maybe improve handling of cases like: ?:# i.e. multiple punctuation concatenated together
-    pattern = abbreviations_regex + "|" \
-              + number_with_separator_regex + "|" \
-              + number_without_separator_regex + "|" \
-              + r'([!"#$%&()*+,-./:;<=>?@\[\\\]^_{|}~]+)'
+        # TODO Maybe improve handling of cases like: ?:# i.e. multiple punctuation concatenated together
+        pattern = abbreviations_regex + "|" \
+                  + number_with_separator_regex + "|" \
+                  + number_without_separator_regex + "|" \
+                  + r'([!"#$%&()*+,-./:;<=>?@\[\\\]^_{|}~]+)'
 
-    result = re.sub(pattern=pattern, repl=repl_punctuation, string=modComm)
+        result = re.sub(pattern=pattern, repl=repl_punctuation, string=modComm)
 
-    # Remove repeated spaces
-    result = re.sub(r' {2,}', repl=' ', string=result)
+        # Remove repeated spaces
+        result = remove_repeated_whitespace(result)
 
-    return result
+        return result
+    except RuntimeWarning as e:
+        # print("Warning: {}".format(e))
+        return modComm
+
+
+def remove_repeated_whitespace(result):
+    return re.sub(r'\s{2,}', repl=' ', string=result).strip()
 
 
 def repl_punctuation(matchobj):
     start = matchobj.start()
     end = matchobj.end()
-    print("Start, end: {}, {}".format(start, end))
+    # print("Start, end: {}, {}".format(start, end))
     match = matchobj.group(0)
 
     if match is not None:
@@ -108,73 +139,84 @@ def repl_punctuation(matchobj):
 
 
 def split_clitics(modComm):
-    verb_tokens = "(ca|had|ai|am|are|could|dare|did|does|do|has|have|is|need|must|ought|should|was|were|wo|would)"
+    try:
+        validate_input(modComm)
+        verb_tokens = "(ca|had|ai|am|are|could|dare|did|does|do|has|have|is|need|must|ought|should|was|were|wo|would)"
 
-    # Handle "verb + not"
-    modComm = re.sub(r"\b" + verb_tokens + r"n't\b", repl=r"\1 n't", string=modComm, flags=re.IGNORECASE)
+        # Handle "verb + not"
+        modComm = re.sub(r"\b" + verb_tokens + r"n't\b", repl=r"\1 n't", string=modComm, flags=re.IGNORECASE)
 
-    # Handle possessives and has/is
-    modComm = re.sub(r"(\w+)'s", repl=r"\1 's", string=modComm, flags=re.IGNORECASE)
+        # Handle possessives and has/is
+        modComm = re.sub(r"(\w+)'s", repl=r"\1 's", string=modComm, flags=re.IGNORECASE)
 
-    # Handle plural possessives
-    modComm = re.sub(r"(\w+)s'", repl=r"\1s '", string=modComm, flags=re.IGNORECASE)
+        # Handle plural possessives
+        modComm = re.sub(r"(\w+)s'", repl=r"\1s '", string=modComm, flags=re.IGNORECASE)
 
-    # Handle 've (e.g. should've)
-    modComm = re.sub(r"(\w*[a-zA-Z])'ve\b", repl=r"\1 've", string=modComm, flags=re.IGNORECASE)
+        # Handle 've (e.g. should've)
+        modComm = re.sub(r"(\w*[a-zA-Z])'ve\b", repl=r"\1 've", string=modComm, flags=re.IGNORECASE)
 
-    # Handle 'm (e.g. I'm)
-    modComm = re.sub(r"\b([Ii])'m\b", repl=r"\1 'm", string=modComm, flags=re.IGNORECASE)
+        # Handle 'm (e.g. I'm)
+        modComm = re.sub(r"\b([Ii])'m\b", repl=r"\1 'm", string=modComm, flags=re.IGNORECASE)
 
-    # Handle 're (e.g. You're)
-    modComm = re.sub(r"(\w*[a-zA-Z])'re\b", repl=r"\1 're", string=modComm, flags=re.IGNORECASE)
+        # Handle 're (e.g. You're)
+        modComm = re.sub(r"(\w*[a-zA-Z])'re\b", repl=r"\1 're", string=modComm, flags=re.IGNORECASE)
 
-    # Handle 'll (e.g. I'll)
-    modComm = re.sub(r"(\w*[a-zA-Z])'ll\b", repl=r"\1 'll", string=modComm, flags=re.IGNORECASE)
+        # Handle 'll (e.g. I'll)
+        modComm = re.sub(r"(\w*[a-zA-Z])'ll\b", repl=r"\1 'll", string=modComm, flags=re.IGNORECASE)
 
-    # Handle 'd (e.g. She'd)
-    modComm = re.sub(r"(\w*[a-zA-Z])'d\b", repl=r"\1 'd", string=modComm, flags=re.IGNORECASE)
+        # Handle 'd (e.g. She'd)
+        modComm = re.sub(r"(\w*[a-zA-Z])'d\b", repl=r"\1 'd", string=modComm, flags=re.IGNORECASE)
 
-    return modComm
+        return modComm
+    except RuntimeWarning as e:
+        # print("Warning: {}".format(e))
+        return modComm
 
 
 def remove_stopwords(modComm):
-    stopwords = read_stopwords()
-
-    stopwords_regex = [sw + r"(?:/[\S$]+)*" for sw in stopwords]
-
-    regex = r"(?:\s|^)(?:" + "|".join(stopwords_regex) + r")(?=\s|$)"
-
-    modComm = re.sub(regex, "", modComm, flags=re.IGNORECASE)
-
-    return re.sub(r'\s{2,}', repl=' ', string=modComm)
+    try:
+        validate_input(modComm)
+        stopwords = read_stopwords()
+        stopwords_regex = [sw + r"(?:/[\S$]+)*" for sw in stopwords]
+        regex = r"(?:\s|^)(?:" + "|".join(stopwords_regex) + r")(?=\s|$)"
+        modComm = re.sub(regex, "", modComm, flags=re.IGNORECASE)
+        return remove_repeated_whitespace(modComm)
+    except RuntimeWarning as e:
+        # print("Warning: {}".format(e))
+        return modComm
 
 
 def separate_sentences(modComm):
-    pn_abb = read_proper_name_abbreviations()
-    non_pn_abb = read_all_abbreviations() - pn_abb
+    try:
+        validate_input(modComm)
+        pn_abb = read_proper_name_abbreviations()
+        non_pn_abb = read_all_abbreviations() - pn_abb
 
-    # The last dot on proper name abbreviation should be removed to make matching easier later
-    # pn_abb = {re.sub(r"(\w+)\.$", r"\1", abb) for abb in pn_abb}
+        # The last dot on proper name abbreviation should be removed to make matching easier later
+        # pn_abb = {re.sub(r"(\w+)\.$", r"\1", abb) for abb in pn_abb}
 
-    pn_abb_regex = "(?P<pn_abb>(" + "|".join(pn_abb).replace(".", "\.") + r")\ +)"
-    non_pn_abb_regex = "(?P<non_pn_abb>(" + "|".join(non_pn_abb).replace(".", "\.") + r")\ +)"
+        pn_abb_regex = "(?P<pn_abb>(" + "|".join(pn_abb).replace(".", "\.") + r")\ +)"
+        non_pn_abb_regex = "(?P<non_pn_abb>(" + "|".join(non_pn_abb).replace(".", "\.") + r")\ +)"
 
-    modComm = re.sub(
-        r'''
-        ((?P<period>\."?)\ *(?=\w+))
-        |
-        {pn_abb}(?=\w+)
-        |
-        {non_pn_abb}(?=\w+)
-        '''.format(pn_abb=pn_abb_regex, non_pn_abb=non_pn_abb_regex),
-        repl_sentence,
-        modComm,
-        flags=re.VERBOSE
-    )
+        modComm = re.sub(
+            r'''
+            ((?P<period>\."?)\ *(?=\w+))
+            |
+            {pn_abb}(?=\w+)
+            |
+            {non_pn_abb}(?=\w+)
+            '''.format(pn_abb=pn_abb_regex, non_pn_abb=non_pn_abb_regex),
+            repl_sentence,
+            modComm,
+            flags=re.VERBOSE
+        )
 
-    modComm = re.sub(r'([!?]+)\ *(?=[A-Z]+)', r"\1\n", modComm)
+        modComm = re.sub(r'([!?]+)\ *(?=[A-Z]+)', r"\1\n", modComm)
 
-    return modComm
+        return modComm
+    except RuntimeWarning as e:
+        # print("Warning: {}".format(e))
+        return modComm
 
 
 def repl_sentence(matchobj):
@@ -195,21 +237,105 @@ def repl_sentence(matchobj):
 
 
 def tag_part_of_speech(modComm):
-    tokens = nlp(modComm)
+    try:
+        validate_input(modComm)
+        tokens = nlp(modComm)
+        tagged_comment = ""
 
-    tagged_comment = ""
+        tagged_comments = list()
 
-    for token in tokens:
-        tag = token.tag_
-        token_length = len(token)
-        idx = token.idx
-        tagged_comment += " " + modComm[idx:idx + token_length] + "/" + tag
+        for token in tokens:
+            tag = token.tag_
 
-    return tagged_comment.strip()
+            if not tag:
+                print("Warning: Spacy didn't produce a tag for this token: {}. Skipping it...".format(token))
+                continue
+
+            token_length = len(token)
+            idx = token.idx
+            tagged_comments.append(modComm[idx:idx + token_length] + "/" + tag)
+
+        tagged_comment = " ".join(tagged_comments)
+
+        return tagged_comment.strip()
+    except RuntimeWarning as e:
+        # print("Warning: {}".format(e))
+        return modComm
+
+
+def remove_pos_tags(text):
+    return re.split(r"\s+", re.sub("(?<=\S)/(?!\S*/\S*)[\S$]+(?=(\s|$))", "", text))
+
+
+def split_by_tag(text):
+    text = remove_repeated_whitespace(text)
+    tokens = re.split(r"\s", text)
+    tokens = [re.split(r"(?<=\S)/(?!\S*/\S*)(?=[\S$]+(?:\s|$))", token) for token in tokens]
+
+    # tokens_dict = dict()
+    #
+    # for token in tokens:
+    #     if len(token) != 2:
+    #         print("Warning: this token didn't have a tag: {}".format(token))
+    #         continue
+    #     tokens_dict[token[0]] = token[1]
+
+    return tokens
 
 
 def apply_lemmatization(modComm):
-    return modComm
+    try:
+        validate_input(modComm)
+        tokens_with_tag = split_by_tag(modComm)
+        # Remove POS tags from previous step
+        modComm_notags = remove_pos_tags(modComm)
+
+        # tokens = nlp(modComm_notags)
+
+        tokens = spacy.tokens.Doc(nlp.vocab, words=modComm_notags)
+        tokens = nlp.tagger(tokens)
+
+        if len(tokens) != len(tokens_with_tag):
+            print("Warning: list sizes don't match")
+
+        lemmatized_comments = list()
+
+        for i, token in enumerate(tokens):
+            lemma = token.lemma_
+
+            if not lemma:
+                print("Warning: Spacy didn't produce a lemma for this token: {}. Skipping it...".format(token))
+                continue
+
+            token_with_tag = tokens_with_tag[i]
+
+            if len(token_with_tag) != 2:
+                print("Warning: Missing tag or token {}. Skipping it...".format(token_with_tag))
+                continue
+
+            tag = token_with_tag[1]
+
+            if not tag:
+                print("Warning: Missing tag for token. Skipping it...")
+                continue
+
+            lemmatized_comments.append(lemma + "/" + tag)
+
+        tagged_comment = " ".join(lemmatized_comments)
+
+        return tagged_comment.strip()
+    except RuntimeWarning as e:
+        # print("Warning: {}".format(e))
+        return modComm
+
+
+def lowercase(modComm):
+    try:
+        validate_input(modComm)
+
+    except RuntimeWarning as e:
+        # print("Warning: {}".format(e))
+        return modComm
 
 
 def preproc1(comment, steps=range(1, 11)):
@@ -223,37 +349,41 @@ def preproc1(comment, steps=range(1, 11)):
         modComm : string, the modified comment 
     '''
 
+    if not comment:
+        # skipping empty comments...
+        return comment
+
     modComm = comment
     if 1 in steps:
-        print('Removing newline characters')
+        # print('Removing newline characters')
         modComm = remove_newlines(modComm)
     if 2 in steps:
-        print("Replacing HTML character codes")
+        # print("Replacing HTML character codes")
         modComm = remove_html_char_codes(modComm)
     if 3 in steps:
-        print('Removing urls')
+        # print('Removing urls')
         modComm = remove_urls(modComm)
     if 4 in steps:
-        print('Splitting punctuation')
+        # print('Splitting punctuation')
         modComm = split_punctuation(modComm)
     if 5 in steps:
-        print('Splitting clitics')
+        # print('Splitting clitics')
         modComm = split_clitics(modComm)
     if 6 in steps:
-        print('Tagging with part-of-speech')
+        # print('Tagging with part-of-speech')
         modComm = tag_part_of_speech(modComm)
     if 7 in steps:
-        print('Removing stop words')
+        # print('Removing stop words')
         modComm = remove_stopwords(modComm)
     if 8 in steps:
-        print('Applying lemmatization')
+        # print('Applying lemmatization')
         modComm = apply_lemmatization(modComm)
     if 9 in steps:
-        print('Adding newline after each sentence')
+        # print('Adding newline after each sentence')
         modComm = separate_sentences(modComm)
     if 10 in steps:
-        print('Lower-casing text')
-        modComm = modComm.lower()
+        # print('Lower-casing text')
+        modComm = lowercase(modComm)
 
     return modComm
 
