2e02453b0fc4897fa50f29c9d8f19e202b99ce50
Simon Rojas <srojas@mahisoft.com>
2018-01-29 17:47:35 -0500

    Update template

--- a1_extractFeatures.py
+++ a1_extractFeatures.py
@@ -5,7 +5,7 @@ import os
 import json
 
 
-def extract1( comment ):
+def extract1(comment):
     ''' This function extracts features from a single comment
 
     Parameters:
@@ -17,23 +17,21 @@ def extract1( comment ):
     print('TODO')
     # TODO: your code here
 
-def main( args ):
 
+def main(args):
     data = json.load(open(args.input))
-    feats = np.zeros( (len(data), 173+1))
+    feats = np.zeros((len(data), 173 + 1))
 
     # TODO: your code here
 
-    np.savez_compressed( args.output, feats)
+    np.savez_compressed(args.output, feats)
 
-    
-if __name__ == "__main__": 
 
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process each .')
     parser.add_argument("-o", "--output", help="Directs the output to a filename of your choice", required=True)
     parser.add_argument("-i", "--input", help="The input JSON file, preprocessed as in Task 1", required=True)
     args = parser.parse_args()
-                 
 
     main(args)
 
