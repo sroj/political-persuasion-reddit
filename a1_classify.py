1b231649ba69fd35335749fe065bd317d21d254b
Simon Rojas <srojas@mahisoft.com>
2018-02-08 16:02:49 -0500

    Throw exception for unrecognized classifier

--- a1_classify.py
+++ a1_classify.py
@@ -206,8 +206,7 @@ def class32(X_train, X_test, y_train, y_test, iBest):
         classifier_15k = build_ada_boost_classifier()
         classifier_20k = build_ada_boost_classifier()
     else:
-        print("ERROR: Unrecognized best classifier: {}".format(iBest))
-        return
+        raise RuntimeError("ERROR: Unrecognized best classifier: {}".format(iBest))
 
     # Step 3.1 already does shuffling, but just for sanity...
     X_train, y_train = sklearn.utils.shuffle(X_train, y_train, random_state=42)
