0c33bee4756ecafb1b7ece6be3486c4ec6f5c88a
Simon Rojas <srojas@mahisoft.com>
2018-02-11 09:59:37 -0500

    Add bonus question

--- a1_classify.py
+++ a1_classify.py
@@ -9,17 +9,20 @@ import sklearn.utils
 from scipy import stats
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import f_classif
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split, KFold
+from sklearn.neighbors import KNeighborsClassifier
 
 a1_3_1_comment = "Linear SVC performed the best. This is not a surprise as linear kernel SVMs have proved to be one " \
                  "of the most effective machine learning models for text classification in the literature."
 
-a1_3_2_comment = "As the training set size increases, the accuracy of the classifier should, in general, improve. " \
-                 "This is because the model has more examples to learn from, and so it's predictions are more likely " \
-                 "to generalize better to unseen examples. This trend is indeed observed in this experiment, " \
-                 "as we can see how the accuracies monotonically increase with the training set size."
+a1_3_2_comment = "As the training set size increases, the accuracy of the classifier should, in general, improve (all " \
+                 "other things being equal). This is because the model has more examples to learn from, and so its " \
+                 "predictions are more likely to generalize better to unseen examples. This trend is indeed observed " \
+                 "in this experiment, as we can see how the accuracies monotonically increase with the training set " \
+                 "size. "
 
 a1_3_3_comment_1 = "This is the comment 1 for part 3.3"
 a1_3_3_comment_2 = "This is the comment 2 for part 3.3"
@@ -342,7 +345,7 @@ def class33(X_train, X_test, y_train, y_test, i, X_1k, y_1k):
        X_test: NumPy array, with the selected testing features
        y_train: NumPy array, with the selected training classes
        y_test: NumPy array, with the selected testing classes
-       i: int, the index of the supposed best classifier (from task 3.1)  
+       i: int, the index of the supposed best classifier (from task 3.1)
        X_1k: numPy array, just 1K rows of X_train (from task 3.2)
        y_1k: numPy array, just 1K rows of y_train (from task 3.2)
     '''
@@ -481,6 +484,39 @@ def class34(filename, i):
     save_csv_file('a1_3.4.csv', rows)
 
 
+def class4_bonus(filename):
+    """
+
+
+    """
+    print("\nStarting question 4 (bonus question)")
+    data = load_data(filename)
+    x = data[:, 0:173]
+    y = data[:, 173]
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=42, shuffle=True)
+
+    classifier = LogisticRegression()
+    classifier.fit(x_train, y_train)
+
+    y_pred = classifier.predict(x_test)
+
+    conf_matrix_log_reg = confusion_matrix(y_test, y_pred)
+    accuracy_log_reg = accuracy(conf_matrix_log_reg)
+
+    print("Logistic Regression accuracy: {}".format(accuracy_log_reg))
+
+    classifier = KNeighborsClassifier()
+    classifier.fit(x_train, y_train)
+
+    y_pred = classifier.predict(x_test)
+
+    conf_matrix_log_reg = confusion_matrix(y_test, y_pred)
+    accuracy_log_reg = accuracy(conf_matrix_log_reg)
+
+    print("Nearest Neighbors accuracy: {}".format(accuracy_log_reg))
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", help="the input npz file from Task 2", required=True)
@@ -493,3 +529,4 @@ if __name__ == "__main__":
     X_1k, y_1k = class32(X_train, X_test, y_train, y_test, iBest)
     class33(X_train, X_test, y_train, y_test, iBest, X_1k, y_1k)
     class34(filename, iBest)
+    class4_bonus(filename)
